8ec2a0a0a98909c84d875969e2bee405c69566b9
bright0220 <jackfortin0220@gmail.com>
2023-02-13 01:10:39 -0500

    setup gh-pages

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    ['@babel/preset-react'],
-    ['@babel/preset-env']
-    ['@vue/cli-plugin-babel/preset', {'jsx': false}],
-  ],
-  plugins: [
-    ['transform-react-jsx'],
-    ['@babel/plugin-syntax-dynamic-import']
+    '@vue/cli-plugin-babel/preset'
   ]
 }
--- vue.config.js
+++ vue.config.js
@@ -1,38 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/DeFiOptions-frontend/'
-    : '/',
-  configureWebpack: {
-        module: {
-            rules: [
-                {
-                    test: /react\/*.js$/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-react'],
-                            plugins: ['transform-react-jsx']
-                        }
-                    }
-                },
-                {
-                    test: /chart.js/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-react'],
-                            plugins: ['transform-react-jsx']
-                        }
-                    }
-                },
-            ]
-        }
-    },
-}
-
-/*
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/'
-}*/
+}
